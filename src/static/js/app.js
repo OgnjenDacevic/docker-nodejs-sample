44210383e669dab48405c102d11ee1853a5e43ca
OgnjenDacevic <ognjen.dacevic@vegait.rs>
2024-05-09 11:24:41 +0200

    V4

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above! App is changed again. V3</p>
+                <p className="text-center">No items yet! Add one above! V4</p>
             )}
             {items.map(item => (
                 <ItemDisplay
