e109559ca038afcde990d7e966cbad0d1dd61c87
OgnjenDacevic <ognjen.dacevic@vegait.rs>
2024-05-20 10:16:58 +0200

    Changed vpc

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -16,15 +16,55 @@ provider "aws" {
   }
 }
 
+module "subnet_addrs" {
+  source = "hashicorp/subnets/cidr"
+  base_cidr_block = var.vpc_cidr
+
+  networks = [
+    {
+      name = "Public subnet 1"
+      new_bits = 2
+    },
+    {
+      name = "Public subnet 2"
+      new_bits = 2
+    },
+    {
+      name = "Public subnet 3"
+      new_bits = 2
+    },
+    {
+      name = "Private subnet 1"
+      new_bits = 8
+    },
+    {
+      name = "Private subnet 2"
+      new_bits = 8
+    },
+    {
+      name = "Private subnet 3"
+      new_bits = 8
+    }
+  ]
+
+  
+
+}
+
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "advanced-workshop-vpc"
-  cidr = var.vpc_cidr
-
-  azs             = var.azs
-  private_subnets = var.private_subnets
-  public_subnets  = var.public_subnets
+  cidr = module.subnet_addrs.base_cidr_block
+  
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = [module.subnet_addrs.networks[3].cidr_block,
+            module.subnet_addrs.networks[4].cidr_block,
+            module.subnet_addrs.networks[5].cidr_block]
+  public_subnets  = [module.subnet_addrs.networks[0].cidr_block,
+            module.subnet_addrs.networks[1].cidr_block,
+            module.subnet_addrs.networks[2].cidr_block]
 
   # NAT gateway
   enable_nat_gateway = true
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,20 +1,9 @@
 
-variable "azs" {
-    type = list(string)
-    default = ["", "", ""]
-}
-
-variable "private_subnets" {
-    type = list(string)
-    default = ["", "", ""]
-}
-
-variable "public_subnets" {
-    type = list(string)
-    default = ["", "", ""]
-}
-
 variable "vpc_cidr" {
     type = string
     default = ""
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
 }
