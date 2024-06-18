d08e535160d00ddcaf06dfd19a030972663de13e
OgnjenDacevic <ognjen.dacevic@vegait.rs>
2024-06-18 12:38:57 +0200

    Fixed indentation

--- infrastructure/acm.tf
+++ infrastructure/acm.tf
@@ -5,12 +5,8 @@ data "aws_route53_zone" "zone" {
 module "acm" {
   source  = "terraform-aws-modules/acm/aws"
   version = "5.0.1"
-
-	domain_name = "ognjen-dacevic.epsilon.devops.sitesstage.com"
-
-	validation_method = "DNS"
-
-	zone_id = data.aws_route53_zone.zone.zone_id
-
-	wait_for_validation = true
+  domain_name = "ognjen-dacevic.epsilon.devops.sitesstage.com"
+  validation_method = "DNS"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  wait_for_validation = true
 }
