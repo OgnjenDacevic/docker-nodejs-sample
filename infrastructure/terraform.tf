23098a0045f5311bf234a4885e84b1129ddf4f67
OgnjenDacevic <ognjen.dacevic@vegait.rs>
2024-06-20 09:54:57 +0200

    Installed remote backend, removed public subnets from eks

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,4 +1,13 @@
 terraform {
+    backend "s3" {
+        bucket         = "ognjen-terraform-backend"
+        key            = "terraform.tfstate"
+        region         = "eu-central-1"
+        dynamodb_table = "terraform-lock"
+        encrypt        = true
+        profile        = "terraform"
+    }
+
     required_providers {
         aws = {
             source = "hashicorp/aws"
